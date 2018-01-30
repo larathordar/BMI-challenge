d5f3175dba86763025f9136a46c59dd3744afa78
FleurTamboezer <ftamboezer@gmail.com>
2018-01-30 15:50:38 +0100

    Working on Imperial BMI calculator

--- spec/bmi_calculator_spec.js
+++ spec/bmi_calculator_spec.js
@@ -13,5 +13,9 @@ describe("BMICalculator", function() {
     calculator.metric_bmi(person);
     expect(person.bmiValue).toEqual(26.01);
   });
-  
+
+  it("calculates BMI for a person using imperial method", function() {
+    calculator.imperial_bmi(person);
+    expect(person.bmiValue).toEqual(26.01);
+  });
 });
--- src/person.js
+++ src/person.js
@@ -3,9 +3,17 @@
 function Person(attr) {
   this.weight = attr.weight;
   this.height = attr.height;
+  this.weight_lb = attr.weight_lb;
+  this.height_inch = attr.height_inch;
+  this.height_feet = attr.height_feet;
 };
 
 Person.prototype.calculate_bmi = function() {
   calculator = new BMICalculator();
   calculator.metric_bmi(this);
   };
+
+  Person.prototype.calculate_bmi_imp = function() {
+    calculator_imp = new BMICalculator();
+    calculator_imp.imperial_bmi(this);
+    };
